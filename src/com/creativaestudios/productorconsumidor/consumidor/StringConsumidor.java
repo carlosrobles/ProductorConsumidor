674832a06d910c341de6f0d7382387378883355b
Carlos Robles <carlos@estudiocarlosrobles.com>
2013-07-10 19:13:27 +0200

    minor changes

--- src/com/creativaestudios/productorconsumidor/consumidor/StringConsumidor.java
+++ src/com/creativaestudios/productorconsumidor/consumidor/StringConsumidor.java
@@ -15,14 +15,13 @@ public class StringConsumidor extends Thread {
 		String temp;
 		while (true) {
 			try {
-				// lamamos  dequeue directamente, porque el consumidor no tiene que
+				// Llamamos  dequeue directamente, porque el consumidor no tiene que
 				// organizarse con nadie, y ni si quiera debe conocer a otros
 				// consumidores o al productor, y mucho menos su implementacion,
 				// solo debe conocer a la cola de la que consume, y saber como
 				// comunicarse con ella. Es la cola la que decide como gestionar
-				// sus recursos por tanto, el consumidor llama, y si la cola
-				// decide si sirve,
-				// pone en espera, o lo que haga falta.
+				// sus recursos por tanto, el consumidor llama, y si cola
+				// decide si sirve, lo pone en espera, o lo que haga falta.
 				temp = buffer.dequeue();
 				System.out.println(getName() + ": " + temp);
 				Thread.sleep(Constants.MINIMO_PAUSA_CONSUMIDOR
